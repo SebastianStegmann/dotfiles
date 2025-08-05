07d151ab32c9ecb9d8cb74210fbda9472bc51031
Sebastian Stegmann <92033571+SebastianStegmann@users.noreply.github.com>
2025-08-05 13:35:53 +0200

    Small changes

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 Managing my dotfiles via stow
 Install this repo and place it in ~/.dotfiles
 
-
 # Fresh install
 1. Install Homebrew
 ```
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,9 +12,11 @@ vim.o.softtabstop = 2
 vim.o.swapfile = false
 vim.o.winborder = "rounded"
 
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
-vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set({'n','v','x'}, '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set({'n','v','x'}, '<leader>w', ':write<CR>')
+vim.keymap.set({'n','v','x'}, '<leader>q', ':quit<CR>')
+vim.keymap.set({'n','v','x'}, '<leader>s', ':e #<CR>')
+vim.keymap.set({'n','v','x'}, '<leader>S', ':sf #<CR>')
 
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
@@ -23,20 +25,12 @@ vim.pack.add({
   { src = "https://github.com/vague2k/vague.nvim" },
   { src = "https://github.com/stevearc/oil.nvim" },
   { src = "https://github.com/echasnovski/mini.pick" },
-  { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/chomosuke/typst-preview.nvim" },
+  { src = "https://github.com/mason-org/mason.nvim" },
+
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end,
-})
-vim.cmd("set completeopt+=noselect")
-
+require "mason".setup()
 require "mini.pick".setup()
 require("oil").setup({
 	default_file_explorer = true,
@@ -64,10 +58,23 @@ require "vague".setup({ transparent = true })
 vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=NONE")
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+--   desc = 'Highlight when yanking text',
+--   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+--   callback = function()
+--     vim.highlight.on_yank()
+--   end,
+-- })
+
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   callback = function(ev)
+--     local client = vim.lsp.get_client_by_id(ev.data.client_id)
+--     if client:supports_method('textDocument/completion') then
+--       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+--     end
+--   end,
+-- })
+-- vim.cmd("set completeopt+=noselect")
+--
+-- vim.print(vim.pack.get())
+-- vim.pack.del({"packagename"})
